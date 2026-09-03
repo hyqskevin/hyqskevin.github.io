eac786b708f3389fdb1fda30a8c2b5b9096d27bf
hyqskevin <hyqskevin@users.noreply.github.com>
2026-09-03 01:36:18 +0000

    deploy: hyqskevin/hyqskevin-hexo@008fc3df18af93eeab0ac6fc348fc8bbe6c38995

--- code/lib/doUpload.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'file.func.php';
-$fileInfo=$_FILES['myFile'];
-var_dump(upload_file($fileInfo));
--- code/lib/download.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'file.func.php';
-$filename=$_GET['filename'];
-down_file1($filename);
